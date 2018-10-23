97f1f6397118f841ca6d9f63db25de66738f4a9b
marcingal <marcin921125@gmail.com>
2018-10-23 15:19:19 +0200

    end of 2nd day

--- script6.js
+++ script6.js
@@ -1,3 +1,5 @@
+// 1 sposób 
+
 let a = [];
 
 for (i=0; i < 8; i++){
@@ -8,3 +10,20 @@ console.log(a);
 
 let b = a.slice(2,6);
 console.log(b);
+
+
+// 2 sposób
+
+var numbers = [];
+var numbersSize = 14;
+for (var i = 0; i < numbersSize; i++) {
+  numbers[i] = parseInt(Math.random() * 10 + 1);
+}
+
+console.log('tablica poczatkowa = ', numbers);
+
+var startIndex = (numbersSize / 2) - 2;
+console.log('startIndex = ', startIndex);
+var numberOfItems = 4;
+var newArray = numbers.slice(startIndex, startIndex + numberOfItems);
+console.log('srodkowe 4 elementy = ', newArray);
